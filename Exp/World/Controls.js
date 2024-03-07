45218444dcefa4d1ebab87ef4ba9c2747ad05607
Wolkenwolf9 <anton.kripp@gmail.com>
2024-03-07 16:51:32 +0100

    Responsive Raum position

--- Exp/World/Controls.js
+++ Exp/World/Controls.js
@@ -1,12 +1,20 @@
+import GSAP from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger.js";
+
 import Experience from "../Exp";
 
 export default class Controls {
   constructor() {
     this.experience = new Experience();
     this.scene = this.experience.scene;
+    this.sizes = this.experience.sizes;
     this.resources = this.experience.resources;
     this.time = this.experience.time;
     this.camera = this.experience.camera;
+    this.room = this.experience.world.room.actualRoom;
+    GSAP.registerPlugin(ScrollTrigger);
+
+    this.setPath();
 
     // this.progress = 0;
     // this.dummyVector = new THREE.Vector3(0, 0, 0);
@@ -22,7 +30,23 @@ export default class Controls {
     // this.onWheel();
   }
 
-  // setPath() {
+  setPath() {
+    console.log(this.room);
+    this.timeline = new GSAP.timeline();
+    this.timeline.to(this.room.position, {
+      x: () => {
+        return this.sizes.width * 0.0011;
+      },
+      scrollTrigger: {
+        trigger: ".first-move",
+        markers: true,
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 0.5,
+        invalidateOnRefresh: true,
+      },
+    });
+  }
   //   this.curve = new THREE.CatmullRomCurve3(
   //     [
   //       new THREE.Vector3(-5, 0, 0),
